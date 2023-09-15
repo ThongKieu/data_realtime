9c98c4f35800fb7e72c7c6dcf5f9fdb41799c8b9
itThoViet <it@thoviet.com.vn>
2023-09-15 11:24:15 +0700

    11

--- app/Http/Controllers/WorksAssignmentController.php
+++ app/Http/Controllers/WorksAssignmentController.php
@@ -23,7 +23,7 @@ class WorksAssignmentController extends Controller
             $today = date('Y-m-d');
         }
         // thông tin điện nước
-        $dien_nuoc = WorksAssignment::where('created_at', 'like',$today.'%')->whereBetween('status_work',[0,3])->get();   
+        $dien_nuoc = WorksAssignment::where('created_at', 'like',$today.'%')->where('')->whereBetween('status_work',[0,3])->get();   
         // Work::where('date_book','=',$today)->where('kind_work','=','0')->where('status_cus','=',1)->get();
 
 
@@ -86,13 +86,13 @@ class WorksAssignmentController extends Controller
         $workHas->save();
         
 
-        $id_work_has = WorksAssignment::where('id_cus', '=', $id_cus)->where('id_worker', '=', $id_worker)->value('id');
+        // $id_work_has = WorksAssignment::where('id_cus', '=', $id_cus)->where('id_worker', '=', $id_worker)->value('id');
 
         // insert to new checkin check out
-        $newio =  new WorksAssignment();
-        // $newio -> id_cus = $id_cus;
-        $newio -> id_work_has = $id_work_has;
-        $newio -> save();
+        // $newio =  new WorksAssignment();
+        // // $newio -> id_cus = $id_cus;
+        // $newio -> id_work_has = $id_work_has;
+        // $newio -> save();
        
 
         // CheckWorkByAdminController::create($id_work_has);
@@ -101,5 +101,6 @@ class WorksAssignmentController extends Controller
 
         // WorkerController::sentNewWorkToWorker($request->get('id_worker'), $info_noti_push);
         // return redirect()->action('WorkController@home');
+        return 'OK';
     }
 }

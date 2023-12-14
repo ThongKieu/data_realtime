3663a259323b8e0f1cde2c45b68537d936afaf23
itThoViet <it@thoviet.com.vn>
2023-12-14 09:43:04 +0700

    14

--- routes/web.php
+++ routes/web.php
@@ -73,6 +73,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    
 });
-
+Route::get('/test-page',function(){return view('welcome');});
 require __DIR__.'/auth.php';

35729f4ced1752aab0e08ad125ac6e18a0c5f561
ThongKieu <kieuthong16dth@gmail.com>
2024-01-25 16:11:24 +0700

    update 25/01/24 update

--- resources/js/Pages/Search.jsx
+++ resources/js/Pages/Search.jsx
@@ -92,47 +92,8 @@ const TABLE_ROWS = [
     },
 ];
 function Search({ auth }) {
-    const [email, setEmail] = useState("");
-    // const onChange = ({ target }) => setEmail(target.value);
-    const [dataReturn, setDataReturn] = useState('');
-    const [keySearch, setKey] = useState('');
-
-    const fetchSearch = async () => {
-
-
-        try {
-            let data = {
-                keySearch: keySearch
-            };
-            const response = await fetch("api/web/search", {
-                method: "POST",
-                body: JSON.stringify(data), // Gửi dữ liệu dưới dạng JSON
-                headers: {
-                    "Content-Type": "application/json", // Xác định loại dữ liệu gửi đi
-                },
-            });
-            // console.log("XIN CHAO DATA ACTIVE:",response.ok);
-
-            if (response.ok) {
-                const responseData = await response.json(); // Convert response to JSON
-                setDataReturn(responseData);
-                console.log('Response Data:', responseData);
-            } else {
-                console.error('Error:', response.status, response.statusText);
-            }
-        } catch (error) {
-            console.log("hihi", error);
-        }
-    };
-
-
-    const handleChange = (e) => {
-        setKey(e.target.value);
-    };
-    console.log(keySearch);
-    useEffect(() => {
-        fetchSearch();
-    }, []);
+    const [email, setEmail] = React.useState("");
+    const onChange = ({ target }) => setEmail(target.value);
     return (
         <AuthenticatedLayout user={auth.user}>
             <Head title="chat" />

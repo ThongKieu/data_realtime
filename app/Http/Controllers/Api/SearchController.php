6674131f3bdcf0c4aa6461c11ee32f8bd302f5e3
ThongKieu <kieuthong16dth@gmail.com>
2024-01-26 17:03:01 +0700

    update 26/01/24 update

--- app/Http/Controllers/Api/SearchController.php
+++ app/Http/Controllers/Api/SearchController.php
@@ -14,7 +14,37 @@ class SearchController extends Controller
     public function index()
     {
 
-        $data = WorksAssignment::where('id','!=','0')->orderBy('id','desc')->limit(100)->get();
+        // $data = WorksAssignment::where('id','!=','0')->orderBy('id','desc')->limit(100)->get();
+        $data = DB::table('works_assignments')
+            ->join('works', 'works_assignments.id_cus', '=', 'works.id')
+            ->join('workers', 'works_assignments.id_worker', '=', 'workers.id')
+            ->where('works_assignments.id','>','0')
+            ->orderBy('works_assignments.id','desc')
+            ->limit(100)->get([
+                "works_assignments.id",
+                "works_assignments.id_cus",
+                "works_assignments.id_worker",
+                "works_assignments.id_phu",
+                "works_assignments.real_note",
+                "works_assignments.spending_total",
+                "works_assignments.income_total",
+                "works_assignments.bill_imag",
+                "works_assignments.seri_imag",
+                "works_assignments.status_work",
+                "works_assignments.check_in",
+                "works_assignments.seri_number",
+                "works_assignments.status_admin_check",
+                "works_assignments.flag_check",
+                "works.work_content",
+                "works.date_book",
+                "works.street",
+                "works.district",
+                "works.phone_number",
+                "works.image_work_path",
+                "works.kind_work",
+                "workers.worker_full_name",
+                // "workers.worker_name",
+                "works.name_cus"]);
         return $data;
     }
     public function searchAjax(Request $request)
@@ -26,7 +56,6 @@ class SearchController extends Controller
             $data = DB::table('works_assignments')
             ->join('works', 'works_assignments.id_cus', '=', 'works.id')
             ->join('workers', 'works_assignments.id_worker', '=', 'workers.id')
-            ->join('warranties','works_assignments.id', '=', 'warranties.id_work_has')
             ->where('works_assignments.id','>','0')
             ->where('works.phone_number','like',$key)
             ->orWhere('works.street','like',$key)
@@ -61,7 +90,6 @@ class SearchController extends Controller
         $data = DB::table('works_assignments')
             ->join('works', 'works_assignments.id_cus', '=', 'works.id')
             ->join('workers', 'works_assignments.id_worker', '=', 'workers.id')
-            ->join('warranties','works_assignments.id', '=', 'warranties.id_work_has')
             ->where('works_assignments.id','>','0')
             ->orderBy('works_assignments.id','desc')
             ->limit(100)->get([

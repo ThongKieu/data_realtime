a384892ddbbb46ef8ff53fa970104d1c0f6c5464
itThoViet <it@thoviet.com.vn>
2024-03-25 14:02:04 +0700

    2503

--- app/Http/Controllers/Api/ChartsController.php
+++ app/Http/Controllers/Api/ChartsController.php
@@ -3,20 +3,25 @@
 namespace App\Http\Controllers;
 
 use App\Models\WorksAssignment;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class ChartsController extends Controller
 {
-    public function TopBannerAdmin() {
+    public function TopBannerAdmin()
+    {
 
+        $today = Carbon::now();
+        $this_m  = $today->format('m-y');
+        // $this_m = '';
+        $befo_m = $today->subMonth(1);;
+        
         $dien_nuoc = WorksAssignment::where('');
-        
     }
-    public function DoanhThuBang() {
-        
+    public function DoanhThuBang()
+    {
     }
-    public function GhiNhanLichBang() {
-        
+    public function GhiNhanLichBang()
+    {
     }
-    
 }

888d7683d919a9909c1de97798150ef5cc47bf52
itThoViet <it@thoviet.com.vn>
2024-04-12 14:08:52 +0700

    123

--- app/Http/Controllers/Api/ReportWorkerController.php
+++ app/Http/Controllers/Api/ReportWorkerController.php
@@ -16,32 +16,32 @@ class ReportWorkerController extends Controller
         if ($id_worker != null) {
             $check = ReportWorker::where('date_do', '=', $date_do)->where('id_worker', '=', $id_worker)->get('work_revenue', 'work_expenditure');
             if (count($check) == 1) {
-                if($work_revenue == null){$work_revenue =0;}
-                if($work_expenditure == null){$work_expenditure =0;}
-
-                foreach ($check as $item)
-                {
-                    $item-> work_revenue = $item-> work_revenue + $work_revenue;
-                    $item-> work_expenditure = $item-> work_expenditure + $work_expenditure;
+                if ($work_revenue == null) {
+                    $work_revenue = 0;
+                }
+                if ($work_expenditure == null) {
+                    $work_expenditure = 0;
                 }
 
-                
-                $up = ReportWorker::where('date_do', '=', $date_do)->where('id_worker', '=', $id_worker)-> update(['work_revenue'=>$item-> work_revenue, 'work_expenditure'=>$item-> work_expenditure]);
-                if($up)
-                {
+                foreach ($check as $item) {
+                    $item->work_revenue = $item->work_revenue + $work_revenue;
+                    $item->work_expenditure = $item->work_expenditure + $work_expenditure;
+                }
+
+
+                $up = ReportWorker::where('date_do', '=', $date_do)->where('id_worker', '=', $id_worker)->update(['work_revenue' => $item->work_revenue, 'work_expenditure' => $item->work_expenditure]);
+                if ($up) {
                     return 1;
                 }
                 return 0;
-            }
-            else
-            {
+            } else {
                 $add_n = new ReportWorker([
                     'id_worker' => $id_worker,
                     'date_do' => $date_do,
                     'work_revenue' => $work_revenue,
                     'work_expenditure' => $work_expenditure,
                 ]);
-                $add_n ->save();
+                $add_n->save();
                 if ($add_n) {
                     return 1;
                 }
@@ -55,12 +55,22 @@ class ReportWorkerController extends Controller
             $date_check = date('Y-m-d');
         }
         if ($r->id_worker != null) {
-            $data = ReportWorker::where('date_do', '=', $date_check)->where('id_worker', '=', $r->id_worker)->get(['id_worker', 'date_do', 'work_revenue', 'work_expenditure']);
-            $data_2 = FuelOTWorker::where('fuel_o_t_workers_date_set', '=', $date_check)->where('fuel_o_t_workers_id', '=', $r->id_worker)->get(['fuel_o_t_workers_content', 'fuel_o_t_workers_spend_money']);
-            if ($data && $data_2) {
-                $r_data = array_merge($data, $data_2);
+            $data = ReportWorker::where('date_do', '=', $date_check)->where('id_worker', '=', '4')->get(['id_worker', 'date_do', 'work_revenue', 'work_expenditure']);
+            $data_2 = FuelOTWorker::where('fuel_o_t_workers_date_set', '=', $date_check)->where('fuel_o_t_workers_id', '=', '4')->get(['fuel_o_t_workers_content', 'fuel_o_t_workers_spend_money']);
+            // dd($data_2);
+
+            if (count($data) > 0 || count($data_2) > 0) {
+                
+                $array1 = json_decode($data, true);
+                $array2 = json_decode($data_2, true);
+
+                // Nối các mảng
+                $r_data = array_merge($array1, $array2);
+                $resultJson = json_encode($r_data);
+            } else {
+                $resultJson = 0;
             }
-            return response() -> json($r_data);
+            return response()->json($resultJson);
         }
         return response()->json(' Vui lòng kiểm tra lại thông tin');
     }

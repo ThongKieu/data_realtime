9b8c04796113a0747816b658ba1ae813047701d5
ThongKieu <kieuthong16dth@gmail.com>
2024-04-24 11:39:11 +0700

    update 24/04/2024-navbar hien thi thu chi

--- resources/js/Pages/Admin/Worker/CodeWorker.jsx
+++ resources/js/Pages/Admin/Worker/CodeWorker.jsx
@@ -1,13 +1,26 @@
 import AuthenticatedLayoutAdmin from "@/Layouts/Admin/AuthenticatedLayoutAdmin";
 
-import { Head } from '@inertiajs/react';
-import React from 'react';
+import { Head } from "@inertiajs/react";
+import React from "react";
 import {
     PlusCircleIcon,
     MapPinIcon,
     UserPlusIcon,
 } from "@heroicons/react/24/outline";
-import { Button, Card, Dialog, DialogBody, DialogFooter, DialogHeader, Typography, Input, Textarea, Spinner, Divider, Switch } from "@material-tailwind/react";
+import {
+    Button,
+    Card,
+    Dialog,
+    DialogBody,
+    DialogFooter,
+    DialogHeader,
+    Typography,
+    Input,
+    Textarea,
+    Spinner,
+    Divider,
+    Switch,
+} from "@material-tailwind/react";
 
 import { useState } from "react";
 import { host } from "@/Utils/UrlApi";
@@ -16,18 +29,20 @@ import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 import { useEffect } from "react";
 function CodeWorker(auth) {
     const [open, setOpen] = useState(false);
-    const [checkCode, setCheckCode] = useState('');
+    const [checkCode, setCheckCode] = useState("");
     const [getData, usersData] = useState([]);
     const handleOpen = () => setOpen(!open);
     const [dataCodeWorker, setFormDataCodeWorker] = useState({
-        code_worker: '', kind_worker: '', descript_code_worker: ''
+        code_worker: "",
+        kind_worker: "",
+        descript_code_worker: "",
     });
     const [changStatus, setchangStatus] = useState({
-        id: '', status: ''
+        id: "",
+        status: "",
     });
     const handleChange = (e) => {
         const { name, value } = e.target;
-
 
         setFormDataCodeWorker({ ...dataCodeWorker, [name]: value });
         // fetchCheckCode(value);
@@ -49,9 +64,8 @@ function CodeWorker(auth) {
                 const resData = await res.json();
                 console.log("re", resData);
                 if (resData.data == 1) {
-                    alert('Mã vừa nhập đã tồn tại vui lòng đổi mã!!! ');
-                }
-                else {
+                    alert("Mã vừa nhập đã tồn tại vui lòng đổi mã!!! ");
+                } else {
                     window.location.reload();
                 }
                 // handleOpen();
@@ -65,15 +79,17 @@ function CodeWorker(auth) {
     };
     const fetchChangSataus = async (changStatus) => {
         try {
-            const res = await fetch(host + "api/web/workers/change-code-status", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(changStatus),
-            });
+            const res = await fetch(
+                host + "api/web/workers/change-code-status",
+                {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(changStatus),
+                }
+            );
             if (res.ok) {
-
                 console.log(res);
                 // Xử lý kết quả trả về từ API nếu cần
             } else {
@@ -141,40 +157,35 @@ function CodeWorker(auth) {
             headerAlign: "center",
         },
         {
-            field: 'status_code_worker',
+            field: "status_code_worker",
             headerName: "Trạng Thái ",
             type: "text",
             width: 220,
             editable: false,
             renderCell: (params) => {
-
                 // pramas.row.status_code_worker
-                const [switches, setSwitches] = useState(params.row.status_code_worker);
-
-                const handleChange = (id) => {
-                    setSwitches({
-                        ...switches,
-                        [id]: !switches[id]
-                    });
+                const [checked, setChecked] = useState(
+                    params.row.status_code_worker === 1
+                );
+                const handleChange = () => {
+                    const newChecked = !checked;
+                    setChecked(newChecked);
+                    // onUpdate(params.row.id, newChecked ? 1 : 0); // Gửi yêu cầu cập nhật dữ liệu tới server
                 };
 
                 // const [checked, setChecked] = useState(false);
 
-                console.log(switches);
+                console.log('checked',checked);
 
                 return (
                     <>
-                        <Switch
-                            id={params.id}
-                            checked={params.row.status_code_worker===1? switches[0]:switches[1]}
-                            onChange={() => handleChange(`${params.row.status_code_worker}`)}
-                        />
-                        <div className="text-sm font-semibold text-gray-700">status_code_worker</div>
+                        <Switch checked={checked} onChange={handleChange} />
+                        <div className="text-sm font-semibold text-gray-700">
+                            status_code_worker
+                        </div>
                     </>
                 );
-
             },
-
         },
     ];
 
@@ -184,22 +195,27 @@ function CodeWorker(auth) {
             <div className="p-1">
                 <Card>
                     <div className="grid grid-cols-5 ">
-                        <div className='col-span-4 m-auto'>
-                            <h2 className="uppercase" >Danh Sách - Mã Code</h2>
+                        <div className="col-span-4 m-auto">
+                            <h2 className="uppercase">Danh Sách - Mã Code</h2>
                         </div>
 
-                        <div className="col-span-1 m-auto"> <PlusCircleIcon className="w-10 h-10" onClick={handleOpen}></PlusCircleIcon>
-
-
+                        <div className="col-span-1 m-auto">
+                            {" "}
+                            <PlusCircleIcon
+                                className="w-10 h-10"
+                                onClick={handleOpen}
+                            ></PlusCircleIcon>
                             <Dialog open={open} handler={handleOpen}>
                                 <DialogHeader>
-                                    <h2 className="m-auto">Nhập thông tin thợ mới</h2>
+                                    <h2 className="m-auto">
+                                        Nhập thông tin thợ mới
+                                    </h2>
                                 </DialogHeader>
                                 <DialogBody divider>
                                     <div className="grid grid-cols-2 gap-2 m-1 ">
                                         <Input
                                             type="text"
-                                            className="shadow-none text-black"
+                                            className="text-black shadow-none"
                                             id="code_worker"
                                             name="code_worker"
                                             value={dataCodeWorker.code_worker}
@@ -217,20 +233,19 @@ function CodeWorker(auth) {
                                             label="Loại Thợ"
                                             required
                                         />
-
-
                                     </div>
                                     <Textarea
                                         type="text"
                                         className="shadow-none "
                                         id="descript_code_worker"
                                         name="descript_code_worker"
-                                        value={dataCodeWorker.descript_code_worker}
+                                        value={
+                                            dataCodeWorker.descript_code_worker
+                                        }
                                         onChange={handleChange}
                                         label="Mô tả loại thợ"
                                         required
                                     />
-
                                 </DialogBody>
 
                                 <DialogFooter>
@@ -242,16 +257,18 @@ function CodeWorker(auth) {
                                     >
                                         <span>Cancel</span>
                                     </Button>
-                                    <Button variant="gradient" color="green" onClick={() => sentData(dataCodeWorker)}>
+                                    <Button
+                                        variant="gradient"
+                                        color="green"
+                                        onClick={() => sentData(dataCodeWorker)}
+                                    >
                                         <span>Con1irm</span>
                                     </Button>
                                 </DialogFooter>
                             </Dialog>
                         </div>
-
                     </div>
                 </Card>
-
             </div>
             <Card className="bg-white ">
                 {isLoading ? (
@@ -279,7 +296,7 @@ function CodeWorker(auth) {
                 )}
             </Card>
         </AuthenticatedLayoutAdmin>
-    )
+    );
 }
 
-export default CodeWorker
+export default CodeWorker;

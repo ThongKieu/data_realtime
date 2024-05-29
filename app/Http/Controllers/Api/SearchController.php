88e98eb0dca647cf3a563a4ee2c1d744999b8808
itThoViet <it@thoviet.com.vn>
2024-05-29 13:18:30 +0700

    13h-15

--- app/Http/Controllers/Api/SearchController.php
+++ app/Http/Controllers/Api/SearchController.php
@@ -18,49 +18,59 @@ class SearchController extends Controller
 
         // $data = WorksAssignment::where('id','!=','0')->orderBy('id','desc')->limit(100)->get();
         $data = WorksAssignment::with([
-        'work'=>function($query){
-            $query->select('id', 
-             'work_content',
-            'work_note',
-            'name_cus',
-            'date_book',
-            'phone_number',
-            'street',
-            'district',
-            'member_read', );
-         },
-        'worker'=>function($query){
-            $query->select('id', 
-            'worker_full_name',      
-            'worker_code', );
-         },
-        'warranty' => function($query) {
-             $query->select(
-                 'id','id_work_has','warranty_time','warranty_info','unit'
-             );
-         }])
-        ->where('id', '>', 0)
-        ->where('status_work','=',2)
-        ->orderBy('id', 'desc')
-        ->limit(100)
-        ->get(
-            [
-                "id",
-                "id_cus",
-                "id_worker",
-                "id_phu",
-                "real_note",
-                "spending_total",
-                "income_total",
-                "bill_imag",
-                "seri_imag",
-                "status_work",
-                "check_in",
-                "seri_number",
-                "status_admin_check",
-                "flag_check"
-            ]
-        );
+            'work' => function ($query) {
+                $query->select(
+                    'id',
+                    'work_content',
+                    'work_note',
+                    'name_cus',
+                    'date_book',
+                    'phone_number',
+                    'street',
+                    'district',
+                    'member_read',
+                );
+            },
+            'worker' => function ($query) {
+                $query->select(
+                    'id',
+                    'worker_full_name',
+                    'worker_code',
+                );
+            },
+            'warranty' => function ($query) {
+                $query->select(
+                    'id',
+                    'id_work_has',
+                    'warranty_time',
+                    'warranty_info',
+                    'unit'
+                );
+            }
+        ])
+            ->whereBetween('status_work', [1, 3])
+            ->where('id', '>', 0)
+            ->where('status_work', '=', 2)
+            ->orderBy('id', 'desc')
+            ->limit(100)
+            ->get(
+                [
+                    "id",
+                    "id_cus",
+                    "id_worker",
+                    "id_phu",
+                    "real_note",
+                    "spending_total",
+                    "income_total",
+                    "bill_imag",
+                    "seri_imag",
+                    "status_work",
+                    "check_in",
+                    "seri_number",
+                    "status_admin_check",
+                    "flag_check"
+                ]
+            );
         return $data;
     }
     public function searchAjax(Request $request)
@@ -70,30 +80,41 @@ class SearchController extends Controller
 
         if ($key !== null && $key !== '') {
             $key = '%' . $key . '%';
-    
+
             // Sử dụng Eloquent để truy vấn với các quan hệ
-            $data = WorksAssignment::with(['work' => function($query) use ($key) {
+            $data = WorksAssignment::with([
+                'work' => function ($query) use ($key) {
                     $query->where('phone_number', 'like', $key)
-                          ->orWhere('street', 'like', $key)
-                          ->select('id', 
-                          'work_content',
-                          'work_note',
-                          'name_cus',
-                          'date_book',
-                          'phone_number',
-                          'street',
-                          'district',);
+                        ->orWhere('street', 'like', $key)
+                        ->select(
+                            'id',
+                            'work_content',
+                            'work_note',
+                            'name_cus',
+                            'date_book',
+                            'phone_number',
+                            'street',
+                            'district',
+                        );
                 },
-                'worker'=>function($query){
-                   $query->select('id', 
-                   'worker_full_name',      
-                   'worker_code', );
-                },
-                'warranty' => function($query) {
+                'worker' => function ($query) {
                     $query->select(
-                        'id','id_work_has','warranty_time','warranty_info','unit'
+                        'id',
+                        'worker_full_name',
+                        'worker_code',
                     );
-                }])
+                },
+                'warranty' => function ($query) {
+                    $query->select(
+                        'id',
+                        'id_work_has',
+                        'warranty_time',
+                        'warranty_info',
+                        'unit'
+                    );
+                }
+            ])
+                ->whereBetween('status_work', [1, 3])
                 ->orderBy('id', 'desc')
                 ->limit(100)
                 ->get([
@@ -110,33 +131,43 @@ class SearchController extends Controller
                     "check_in",
                     "seri_number",
                     "status_admin_check",
-                    "flag_check"
+                    "flag_check", 'his_work'
                 ]);
-    
+
             return response()->json($data);
         } else {
             $data = WorksAssignment::with([
-                'work'=>function($query){
-                $query->select('id', 
-                'work_content',
-                'work_note',
-                'name_cus',
-                'date_book',
-                'phone_number',
-                'street',
-                'district',
-                'member_read', );
-                },
-                'worker'=>function($query){
-                    $query->select('id', 
-                    'worker_full_name',      
-                    'worker_code', );
-                },
-                'warranty' => function($query) {
+                'work' => function ($query) {
                     $query->select(
-                        'id','id_work_has','warranty_time','warranty_info','unit'
+                        'id',
+                        'work_content',
+                        'work_note',
+                        'name_cus',
+                        'date_book',
+                        'phone_number',
+                        'street',
+                        'district',
+                        'member_read',
                     );
-                }])
+                },
+                'worker' => function ($query) {
+                    $query->select(
+                        'id',
+                        'worker_full_name',
+                        'worker_code',
+                    );
+                },
+                'warranty' => function ($query) {
+                    $query->select(
+                        'id',
+                        'id_work_has',
+                        'warranty_time',
+                        'warranty_info',
+                        'unit'
+                    );
+                }
+            ])
+                ->whereBetween('status_work', [1, 3])
                 ->orderBy('id', 'desc')
                 ->limit(100)
                 ->get([
@@ -155,76 +186,66 @@ class SearchController extends Controller
                     "status_admin_check",
                     "flag_check"
                 ]);
-    
+
             return response()->json($data);
         }
-       
     }
 
-   public function createWarrantyFromSearch(Request $request )
-   {
+    public function createWarrantyFromSearch(Request $request)
+    {
         $id_cus = $request->id_cus;
-        $data = Work::where('id','=',$id_cus)->get();
+        $data = Work::where('id', '=', $id_cus)->get();
         $date = date('Y-m-d');
-        foreach ($data as $item)
-        {
-            $work_content = 'BH - '. $item -> work_content;
-            if($item->work_note != null)
-            {
-                $note =$item->date_book .'-' . $request->worker_full_name .' - '. $item -> work_note ;
-            }
-            else
-            {
-                $note =$item->date_book .'-' . $request->worker_full_name;
+        foreach ($data as $item) {
+            $work_content = 'BH - ' . $item->work_content;
+            if ($item->work_note != null) {
+                $note = $item->date_book . '-' . $request->worker_full_name . ' - ' . $item->work_note;
+            } else {
+                $note = $item->date_book . '-' . $request->worker_full_name;
             }
             $w = new Work([
-            'work_content' => $work_content,
-            'work_note' => $note,
-            'name_cus' => $item -> name_cus,
-            'date_book' => $date,
-            'phone_number' => $item -> phone_number,
-            'street' => $item -> street,
-            'district' => $item -> district,
-            'kind_work' => $item -> kind_work,
-            'status_cus'=> $item -> status_cus,
-            'image_work_path'=> $item -> image_work_path
+                'work_content' => $work_content,
+                'work_note' => $note,
+                'name_cus' => $item->name_cus,
+                'date_book' => $date,
+                'phone_number' => $item->phone_number,
+                'street' => $item->street,
+                'district' => $item->district,
+                'kind_work' => $item->kind_work,
+                'status_cus' => $item->status_cus,
+                'image_work_path' => $item->image_work_path
             ]);
-            $w ->save();
-            $id = Work::where('phone_number', '=', $item -> phone_number)->where('work_content' ,'=', $work_content)-> orderBy('id','desc')->value('id');
+            $w->save();
+            $id = Work::where('phone_number', '=', $item->phone_number)->where('work_content', '=', $work_content)->orderBy('id', 'desc')->value('id');
 
-            if($id != null)
-            {
-                $id_worker = Worker::where('worker_code','=',$request->worker_code)->value('id');
+            if ($id != null) {
+                $id_worker = Worker::where('worker_code', '=', $request->worker_code)->value('id');
 
 
                 $new = new WorksAssignment([
-                    'id_cus'=> $id,
-                    'id_worker'=>$id_worker,
-                    'status_work'=>'4',
-                    'real_note'=>$note,
+                    'id_cus' => $id,
+                    'id_worker' => $id_worker,
+                    'status_work' => '4',
+                    'real_note' => $note,
                 ]);
-                $new -> save();
+                $new->save();
 
                 return 'Done !!!';
             }
-
         }
         return $request->all();
-   }
-   public function getWarraties(Request $request) {
-     if($request->id)
-     {
-        $warran= Warranties::where('id_work_has','=',$request->id)->get();
-        if(count($warran) > 0 )
-        {
-            return $warran;
-
+    }
+    public function getWarraties(Request $request)
+    {
+        if ($request->id) {
+            $warran = Warranties::where('id_work_has', '=', $request->id)->get();
+            if (count($warran) > 0) {
+                return $warran;
+            } else {
+                return null;
+            }
+        } else {
+            return "No ID found!";
         }
-        else
-        {
-            return null;
-        }
-     }
-     else{ return "No ID found!";}
-   }
+    }
 }
